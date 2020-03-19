52d33c75243dc0022c5594496a0d2449bfec3494
vavalm <valentin@maupin.eu>
2020-03-19 19:00:52 +0100

    - add script to automatize results computation - results dir created

---
+++ src/compute_sol.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+scriptsinstances=/home/vavalm/PycharmProjects/projet_ap/JSPLIB/instances/
+scriptspython=/home/vavalm/PycharmProjects/projet_ap/src/
+resultat=/home/vavalm/PycharmProjects/projet_ap/results/GA/
+
+cd $scriptsinstances
+for instance in ft06*;
+do
+  echo  $instance;
+  cd /home/vavalm/PycharmProjects/projet_ap/src
+  python $scriptspython/GA.py --instance_name $instance > $resultat/$instance.txt;
+  echo  "$instance done";
+done
+
+echo  "That's finished";
